8371960efe95e745aaafdc368e3bd49176d8e95e
chobi0228desu <chobi0228ss@gmail.com>
2024-10-14 16:50:19 +0900

    タイトル設定

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Top from './components/Top';
+import { APPLICATON_TITLT } from './components/common/phrase';
 const App: React.FC = () => {
   return (
     <Router>
