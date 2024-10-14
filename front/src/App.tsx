b9a79689bdc3d3ee4a8c14bc04f99f7670839fa2
chobi0228desu <chobi0228ss@gmail.com>
2024-10-14 21:09:30 +0900

    add : ログインしてない時のみTOPページが表示

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Top from './components/Top';
 import Header from './components/layouts/header';
 const App: React.FC = () => {
+  const [loginFlag, setLoginFlag] = useState(false);
+  //////////////////////////////////////////////////
+  // TODO ここでログインしてるかどうか確認する処理を書く。//
+  //////////////////////////////////////////////////
   return (
-    <div className={`w-full min-h-screen overflow-hidden`}>
+    <div className="w-full min-h-screen overflow-hidden">
       <Header/>
       <Router>
         <Routes>
+          {loginFlag ? (
+            <h1>ログイン済みです。</h1>
+          ) : (
             <Route path="/" element={<Top />} />
+          )}
         </Routes>
       </Router>
     </div>
