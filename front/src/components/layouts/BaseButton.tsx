c8d7f58967e2f78aecf12f6d18e6e58b1b77fd20
chobi0228desu <chobi0228ss@gmail.com>
2024-10-14 22:58:23 +0900

    edit : フォバーした時のボタンの色

--- front/src/components/layouts/BaseButton.tsx
+++ front/src/components/layouts/BaseButton.tsx
@@ -5,7 +5,7 @@ const BaseButton: React.FC<{ to: string; text: string }> = ({ to, text }) => {
   return (
       <Link
         to={to}
-        className="inline-block px-6 py-3 mt-4 text-lg font-semibold text-white bg-blue-900 rounded-lg shadow-lg transition duration-300 ease-in-out hover:bg-blue-600 hover:shadow-xl"
+        className="inline-block px-10 py-3 mt-4 text-lg font-semibold text-white bg-blue-800 rounded-lg shadow-lg transition duration-300 ease-in-out hover:bg-blue-400 hover:shadow-xl"
       >
         {text}
       </Link>
