53444f4755c87f7d1876b279fe6073cff87149cc
chobi0228desu <chobi0228ss@gmail.com>
2024-10-16 22:04:40 +0900

    edit : baseボタンの色

--- front/src/components/layouts/BaseButton.tsx
+++ front/src/components/layouts/BaseButton.tsx
@@ -5,7 +5,7 @@ const BaseButton: React.FC<{ to: string; text: string }> = ({ to, text }) => {
   return (
       <Link
         to={to}
-        className={`inline-block px-10 py-3 mt-4 text-lg font-semibold text-white bg-emerald-700 rounded-lg shadow-lg transition duration-300 ease-in-out hover:bg-emerald-500 hover:shadow-xl`}
+        className={`inline-block px-10 py-3 mt-4 text-lg font-semibold text-white bg-orange-400 rounded-lg shadow-lg transition duration-300 ease-in-out hover:bg-orange-300 hover:shadow-xl`}
       >
         {text}
       </Link>
