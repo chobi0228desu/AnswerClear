44ec6da44656e86cfa3391a458842f17cc579195
chobi0228desu <chobi0228ss@gmail.com>
2024-10-17 20:42:18 +0900

    edit : 新規登録ボタン

--- front/src/components/Signup.tsx
+++ front/src/components/Signup.tsx
@@ -105,7 +105,7 @@ const Signup: React.FC = () => {
 
         <button
           type="submit"
-          className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300"
+          className="w-full mt-6 bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300"
         >
           登録
         </button>
